f851129f3363865a2c1265b750ce6902dcd9d3b6
Artur K <artex199@gmail.com>
2016-11-03 21:50:50 +0100

    Initial android commit.

---
+++ implementation/android/app/src/main/java/pl/bialateam/wordstorm/pojo/Category.java
@@ -0,0 +1,27 @@
+package pl.bialateam.wordstorm.pojo;
+
+/**
+ * Created by Artur on 03.11.2016.
+ */
+
+public class Category {
+
+    String id;
+    String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ implementation/android/app/src/main/java/pl/bialateam/wordstorm/pojo/Collection.java
@@ -0,0 +1,54 @@
+package pl.bialateam.wordstorm.pojo;
+
+/**
+ * Created by Artur on 03.11.2016.
+ */
+
+public class Collection {
+
+    String id;
+    String name;
+    boolean isPublic;
+    int difficultLevel;
+    Category category;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public int getDifficultLevel() {
+        return difficultLevel;
+    }
+
+    public void setDifficultLevel(int difficultLevel) {
+        this.difficultLevel = difficultLevel;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
